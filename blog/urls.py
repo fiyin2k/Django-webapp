3fb7990a3ab6d5a103f236433e104f78f87e4eb7
fiyin2k <fiyinolatunji@yahoo.com>
2023-03-13 01:28:06 +0100

    added  HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+'''
+we're now assigning a view called post_list to the root URL. 
+This URL pattern will match an empty string and the Django URL resolver will i
+gnore the domain name (i.e., http://127.0.0.1:8000/) that prefixes the full URL path. 
+This pattern will tell Django that views.post_list is the right place to go 
+if someone enters your website at the 'http://127.0.0.1:8000/' address.'''
+''''''
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
