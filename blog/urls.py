32c2ec2960af3606de2f764f79794fa7c49a39f8
fiyin2k <fiyinolatunji@yahoo.com>
2023-03-14 18:48:26 +0100

    update: form and validation

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+
 '''
 we're now assigning a view called post_list to the root URL. 
 This URL pattern will match an empty string and the Django URL resolver will i
@@ -10,4 +11,7 @@ if someone enters your website at the 'http://127.0.0.1:8000/' address.'''
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+
 ]
